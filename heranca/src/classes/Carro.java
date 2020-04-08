607e21d37ea104a1ddc43bbdaa21e6f9f81ce778
IcaroGSiqueira <icaro.siqueira@sou.ucpel.edu.br>
2020-04-07 21:04:39 -0300

    organizando classes

--- heranca/src/classes/Carro.java
+++ heranca/src/classes/Carro.java
@@ -1,12 +1,11 @@
-package app;    
+package classes;    
 
 public class Carro extends Veiculo{
     private boolean arCondicionado;
     private boolean bancoCouro;
     private int tamanhoPortaMalas;
     
-    public void marchaRe () {
+    public void marchaRe(){
         System.out.println("O carro está andando de ré.");
-     }
-
+    }
 }
--- heranca/src/classes/Veiculo.java
+++ heranca/src/classes/Veiculo.java
@@ -1,17 +1,17 @@
-package app;
+package classes;
 
-public class Veiculo {
+public class Veiculo{
     private String marca;
     private String nome;
     private double valor;
     private double consumo;
     private boolean ligado;
     
-    public void ligar () {
-        ligado = true;
+    public void ligar(){
+        this.ligado = true;
     }
     
-    public void desligar() {
-        ligado = false;
+    public void desligar(){
+        this.ligado = false;
     }
 }
