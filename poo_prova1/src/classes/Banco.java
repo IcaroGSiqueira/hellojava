babc1a95b7009a845d1192450a62c132f15d078b
IcaroGSiqueira <icaro.siqueira@sou.ucpel.edu.br>
2020-04-28 21:29:32 -0300

    Terminado Banco

---
+++ poo_prova1/src/classes/Banco.java
@@ -0,0 +1,44 @@
+package classes;
+import java.util.ArrayList;
+
+public class Banco{
+    private String banco;
+    ArrayList<ContaCorrente> contas = new ArrayList<>();
+
+    public Banco(String banco){
+        this.banco = banco;
+    }
+
+    public String getBanco(){
+        return this.banco;
+    }
+
+    public void addConta(ContaCorrente conta){
+        contas.add(conta);
+    }
+
+    public void delConta(int conta){
+        contas.remove(conta);
+    }
+
+    public void depositar(int cc, double valor) {
+       contas.get(cc).depositar(valor);
+    }
+
+    public void sacar(int cc, double valor) {
+        contas.get(cc).sacar(valor);
+    }
+
+    public int totConta() {
+        int num = 0;
+        for(ContaCorrente cont : contas){
+            num ++;
+        }
+        return num;
+    }
+    
+    @Override
+    public String toString(){
+        return "Banco: " + this.banco + "\n" + contas.toString();
+    }
+}
