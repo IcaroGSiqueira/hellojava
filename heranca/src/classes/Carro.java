144727a6e0cce24735b3d989011a7c33169b264d
IcaroGSiqueira <icaro.siqueira@sou.ucpel.edu.br>
2020-05-26 22:31:43 -0300

    removendo erros

--- heranca/src/classes/Carro.java
+++ heranca/src/classes/Carro.java
@@ -1,24 +1,17 @@
 package classes;    
 
 public class Carro extends Veiculo{
-    private boolean arCondicionado;
-    private boolean bancoCouro;
-    private int tamanhoPortaMalas;
-
-    public Carro(String marca){
+    public Carro(String marca) {
         super(marca);
     }
-    
-    public void arCondicionado(){
-        this.arCondicionado = true;
+
+    public void arCondicionado() {
     }
 
-    public void bancoCouro(){
-        this.bancoCouro = true;
+    public void bancoCouro() {
     }
 
-    public void tamanhoPortaMalas(int tamanho){
-        this.tamanhoPortaMalas = tamanho;
+    public void tamanhoPortaMalas(int tamanho) {
     }
 
     public void marchaRe(){
